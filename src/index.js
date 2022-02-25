1fc443a0566be26c887d2379a37bb735ef90237c
Alejandro Hernández <alejandroohr@gmail.com>
2022-02-25 15:00:00 -0600

    Adding component to page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './App'
+import App from './App';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Router>
